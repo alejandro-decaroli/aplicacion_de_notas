96fb71a285e2bae46e08a0e17d7946120e3ce4a4
Alejandro <alejandro.decaroli.97@gmail.com>
2025-08-10 17:09:25 -0300

    Se pueden crear notas

--- tauri-app/src-tauri/src/main.rs
+++ tauri-app/src-tauri/src/main.rs
@@ -1,6 +1,9 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use tauri_app_lib::run;
+
 fn main() {
-    tauri_app_lib::run()
+    // Ahora `run` está en el scope y puede ser llamado directamente.
+    run();
 }
--- tauri-app/src/main.ts
+++ tauri-app/src/main.ts
@@ -1,22 +1,82 @@
-import { invoke } from "@tauri-apps/api/core";
+import { exit } from '@tauri-apps/plugin-process';
+import { ask, message } from '@tauri-apps/plugin-dialog';
+import { create, exists, open, mkdir } from '@tauri-apps/plugin-fs';
+import * as path from '@tauri-apps/api/path';
 
-let greetInputEl: HTMLInputElement | null;
-let greetMsgEl: HTMLElement | null;
+let exitButtonEl: HTMLButtonElement | null;
+let backButtonEl: HTMLButtonElement | null;
+let noteFormEl: HTMLFormElement | null;
+let noteName: HTMLInputElement | null;
+let noteContent: HTMLTextAreaElement | null;
+let baseDir: string;
 
-async function greet() {
-  if (greetMsgEl && greetInputEl) {
-    // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-    greetMsgEl.textContent = await invoke("greet", {
-      name: greetInputEl.value,
-    });
+async function goodBye() {
+  let result = await ask("¿Estás seguro de querer salir?");
+  if (result) {
+    exit();
   }
 }
 
+function back() {
+    window.location.href = "../index.html";
+}
+
+async function crearNoteAgain() {
+  let result = await ask("Nota creada exitosamente, ¿Quieres crear otra nota?");
+  if (result) {
+    window.location.href = "creanota.html";
+  } else {
+    window.location.href = "index.html";
+  }
+}
+
+async function createNote(noteName: string, noteContent: string) {
+  try {  
+    baseDir = await path.documentDir() + "/notes";
+    if (!await exists(baseDir)) {
+      await mkdir(baseDir);
+    }
+    let file_path = await path.join(baseDir!, noteName + ".txt");
+    if (await exists(file_path)) {
+      ask("El archivo ya existe, ¿desea sobreescribirlo?").then((result) => {
+        if (result) {
+          open(file_path, { write: true }).then((file) => {
+            file.write(new TextEncoder().encode(noteContent));
+            file.close();
+            crearNoteAgain();
+          });
+        }
+      });
+    } else {
+
+    let file = await create(file_path);
+    await file.write(new TextEncoder().encode(noteContent));
+    await file.close();
+    crearNoteAgain();
+    }
+  } catch (error) {
+    console.error(error);
+    message("Error al crear la nota " + error, { kind: "error" });
+  }
+}
+
+
+
+
 window.addEventListener("DOMContentLoaded", () => {
-  greetInputEl = document.querySelector("#greet-input");
-  greetMsgEl = document.querySelector("#greet-msg");
-  document.querySelector("#greet-form")?.addEventListener("submit", (e) => {
+  exitButtonEl = document.querySelector(".exit-button");
+  exitButtonEl?.addEventListener("click", () => {
+    goodBye();
+  });
+  backButtonEl = document.querySelector(".back-button");
+  backButtonEl?.addEventListener("click", () => {
+    back();
+  });
+  noteFormEl = document.querySelector(".note-form");
+  noteName = document.querySelector("#note-name");  
+  noteContent = document.querySelector("#note-content");
+  noteFormEl?.addEventListener("submit", (e) => {
     e.preventDefault();
-    greet();
+    createNote(noteName!.value, noteContent!.value);
   });
 });
